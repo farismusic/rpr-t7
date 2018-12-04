eacfd6ed5ecd6697a92af334b797aaaedf4c6ee2
farismusic <44246682+farismusic@users.noreply.github.com>
2018-12-05 00:16:56 +0100

    Gotova priprema

---
+++ src/ba/unsa/rpr/tutorijal7/UN.java
@@ -0,0 +1,21 @@
+package ba.unsa.rpr.tutorijal7;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UN {
+    private List<Drzava> drzave = new ArrayList<Drzava>();
+
+    public UN(){
+
+    }
+
+
+    public List<Drzava> getDrzave() {
+        return drzave;
+    }
+
+    public void setDrzave(List<Drzava> drzave) {
+        this.drzave = drzave;
+    }
+}
